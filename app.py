d64de3c5be7491299e98d544113ac16e6e86e5f7
hlgirard <hlgirard@users.noreply.github.com>
2020-03-28 15:54:39 -0400

    Enabled bootstrap css

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 
 # Dash
@@ -7,10 +8,11 @@ import dash_core_components as dcc
 import dash_html_components as html
 import plotly.express as px
 
+PATH_TO_SPEEDTEST_LOG = os.environ.get('PATH_TO_SPEEDTEST_LOG') or '/home/osmc/speedlog.csv'
+
 def df():
-    path_to_log = '/home/osmc/speedlog.csv'
     header = ['Server ID','Sponsor','Server Name','Timestamp','Distance','Ping','Download','Upload','Share','IP Address']
-    return pd.read_csv(path_to_log, names=header)
+    return pd.read_csv(PATH_TO_SPEEDTEST_LOG, names=header)
 
 #########################
 # Dashboard Layout / View
@@ -18,9 +20,6 @@ def df():
 
 # Create the Dash app and get bootstrap CSS
 app = dash.Dash()
-app.css.append_css({
-    "external_url": "bootstrap.min.css"
-})
 
 # Main layout
 app.layout = html.Div([
@@ -31,24 +30,12 @@ app.layout = html.Div([
             html.H1(
                 'Internet Speed Monitor',
                 style = {'font-family': 'Helvetica',
-                       "margin-top": "25",
+                       "margin-top": "50",
                        "margin-bottom": "0",
                        "padding-left": 30},
-                className = 'col-9',
-            ),
-            html.P(
-                'Displays the historic internet speed.',
-                style = 
-                    {
-                        'font-family': 'Helvetica',
-                        "font-size": "120%",
-                        "width": "80%",
-                        'padding-left': 30,
-                    },
-                className='col-9',
             )
         ],
-        className = 'row'
+        className = 'row justify-content-center mt-3'
     ),
 
     # Update Button
@@ -59,39 +46,34 @@ app.layout = html.Div([
                 html.Button(
                     'Update',
                     id = "button-update",
-                    style = {'margin': 'auto', 'margin-top': '25'},
+                    style = {'margin': 'auto', 'margin-top': '50', 'margin-bottom': '50'},
                     className = "btn btn-primary"
                 )
-            ], className = "col-5"),
+            ]),
         ],
-        className = 'row'
+        className = 'row justify-content-center mt-3 mb-3'
     ),
 
-    # Graph
+    # Plot Download
     html.Div([
+        dcc.Graph(
+            id = "plot_download",
+            style = {'margin-top': '20'},
+            figure = px.scatter(df(), x='Timestamp', y='Download', color='Server Name'),
+            animate = True
+        )
+    ], className = "row justify-content-center"),
 
-        # Plot Download
-        html.Div([
-            dcc.Graph(
-                id = "plot_download",
-                style = {'margin-top': '20'},
-                figure = px.scatter(df(), x='Timestamp', y='Download', color='Server Name'),
-                animate = True
-            )
-        ], className = "col-4"),
+    # Plot Upload
+    html.Div([
+        dcc.Graph(
+            id = "plot_upload",
+            style = {'margin-top': '20'},
+            figure = px.scatter(df(), x='Timestamp', y='Upload', color='Server Name'),
+            animate = True
+        )
+    ], className = "row justify-content-center"),
 
-        # Plot Upload
-        html.Div([
-            dcc.Graph(
-                id = "plot_upload",
-                style = {'margin-top': '20'},
-                figure = px.scatter(df(), x='Timestamp', y='Upload', color='Server Name'),
-                animate = True
-            )
-        ], className = "col-4"),
-
-
-        ], className = "col-8"),
 
     dcc.Interval(
         id='interval-component',
@@ -99,7 +81,7 @@ app.layout = html.Div([
         n_intervals=0
         )
 
-], className = '.container')
+], className = '.container col-xl')
 
 ##### Graph Update #####
 
