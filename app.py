82479c6ae351ccc44816ac1c02274bd140217eac
hlgirard <hlgirard@users.noreply.github.com>
2020-03-28 17:27:47 -0400

    debug to false

--- app.py
+++ app.py
@@ -105,7 +105,7 @@ def display_plot(n_clicks, n_intervals):
 if __name__ == '__main__':
 
     app.run_server(
-        debug=True,
+        debug=False,
         host='0.0.0.0',
         port=8050
 )
